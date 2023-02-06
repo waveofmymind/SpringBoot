70c9d7e595d72c0997b25d195a24f8ff466ad2a3
waveofmymind <sjun0913@gmail.com>
2023-02-06 11:29:04 +0900

    fix: dispatcherServlet

--- src/main/java/wave/helloboot/HelloController.java
+++ src/main/java/wave/helloboot/HelloController.java
@@ -1,12 +1,24 @@
 package wave.helloboot;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@Controller
 public class HelloController {
 
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
         return helloService.sayHello(Objects.requireNonNull(name)); //name이 null인 경우 예외 발생
     }
 
--- src/main/java/wave/helloboot/HellobootApplication.java
+++ src/main/java/wave/helloboot/HellobootApplication.java
@@ -17,41 +17,28 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.refresh();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
 
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//인증, 보안, 다국어, 공통 기능
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else if (req.getRequestURI().equals("/user")) {
-						//
-					}
-					else{
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*"); // /이후에 들어오는 모든 요청 다 받아들임
-		});
-		webServer.start();
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(this))
+						.addMapping("/*"));
+				webServer.start();
+			}};
+			applicationContext.registerBean(HelloController.class);
+			applicationContext.registerBean(SimpleHelloService.class);
+			applicationContext.refresh();
 	}
-
 }
+
