d8e32a99632a3aeed63f597dc4d6a4d20aeba3bc
waveofmymind <sjun0913@gmail.com>
2023-02-09 09:52:01 +0900

    [수정] Decorator 패턴 적용 및 커스텀 애노테이션 적용

---
+++ src/main/java/wave/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,17 @@
+package wave.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ComponentScan
+@Configuration
+public @interface MySpringBootAnnotation {
+}
